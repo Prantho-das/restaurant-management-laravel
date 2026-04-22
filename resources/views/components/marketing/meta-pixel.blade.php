e322b72b110fdb451734d467efdc3bb5cb3bccd2
Prantho-das <pranthokumardas786@gmail.com>
2026-04-23 03:06:11 +0600

    feat: add Meta Pixel and GTM event tracking component for conversion monitoring

--- resources/views/components/marketing/meta-pixel.blade.php
+++ resources/views/components/marketing/meta-pixel.blade.php
@@ -16,6 +16,14 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 fbq('init', '{{ $pixelId }}');
 fbq('track', 'PageView');
 
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id={{ $pixelId }}&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+@endif
+
+<script>
 // Listen for custom conversion events from Livewire
 window.addEventListener('conversion-event', function(event) {
     if (event.detail && event.detail.name) {
@@ -23,72 +31,76 @@ window.addEventListener('conversion-event', function(event) {
         const data = event.detail.data || {};
 
         // Meta Pixel
-        if (event.detail.data) {
-            fbq('track', name, data);
-        } else {
-            fbq('track', name);
+        @if($pixelId)
+        if (typeof fbq !== 'undefined') {
+            if (event.detail.data) {
+                fbq('track', name, data);
+            } else {
+                fbq('track', name);
+            }
         }
+        @endif
 
         // Google Tag Manager / GA4
-        if (typeof dataLayer !== 'undefined') {
-            // Mapping strictly to requested GTM names
-            if (name === 'AddToCart') {
-                dataLayer.push({
-                    event: 'add_to_cart',
-                    ecommerce: {
-                        value: data.value,
-                        currency: data.currency,
-                        items: [{
-                            item_id: data.content_ids ? data.content_ids[0] : '',
-                            item_name: data.content_name || '',
-                            price: data.value,
-                            quantity: 1
-                        }]
-                    }
-                });
-            } else if (name === 'BeginCheckout') {
-                dataLayer.push({
-                    event: 'begin_checkout',
-                    ecommerce: {
-                        value: data.value,
-                        currency: data.currency,
-                        items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
-                    }
-                });
-            } else if (name === 'ViewContent') {
-                dataLayer.push({
-                    event: 'view_item',
-                    ecommerce: {
-                        items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
-                    }
-                });
-            } else if (name === 'AddPaymentInfo') {
-                dataLayer.push({
-                    event: 'add_payment_info',
-                    ecommerce: {
-                        value: data.value,
-                        currency: data.currency,
-                        payment_type: data.payment_type || '',
-                        items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
-                    }
-                });
-            } else if (name === 'Purchase') {
-                dataLayer.push({
-                    event: 'purchase',
-                    ecommerce: {
-                        transaction_id: data.transaction_id || '',
-                        value: data.value,
-                        currency: data.currency,
-                        items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
-                    }
-                });
-            }
+        window.dataLayer = window.dataLayer || [];
+        
+        // Mapping strictly to requested GTM names
+        if (name === 'AddToCart') {
+            window.dataLayer.push({ ecommerce: null });
+            window.dataLayer.push({
+                event: 'add_to_cart',
+                ecommerce: {
+                    value: data.value,
+                    currency: data.currency,
+                    items: [{
+                        item_id: data.content_ids ? data.content_ids[0] : '',
+                        item_name: data.content_name || '',
+                        price: data.value,
+                        quantity: 1
+                    }]
+                }
+            });
+        } else if (name === 'BeginCheckout') {
+            window.dataLayer.push({ ecommerce: null });
+            window.dataLayer.push({
+                event: 'begin_checkout',
+                ecommerce: {
+                    value: data.value,
+                    currency: data.currency,
+                    items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
+                }
+            });
+        } else if (name === 'ViewContent') {
+            window.dataLayer.push({ ecommerce: null });
+            window.dataLayer.push({
+                event: 'view_item',
+                ecommerce: {
+                    items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
+                }
+            });
+        } else if (name === 'AddPaymentInfo') {
+            window.dataLayer.push({ ecommerce: null });
+            window.dataLayer.push({
+                event: 'add_payment_info',
+                ecommerce: {
+                    value: data.value,
+                    currency: data.currency,
+                    payment_type: data.payment_type || '',
+                    items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
+                }
+            });
+        } else if (name === 'Purchase') {
+            window.dataLayer.push({ ecommerce: null });
+            window.dataLayer.push({
+                event: 'purchase',
+                ecommerce: {
+                    transaction_id: data.transaction_id || '',
+                    value: data.value,
+                    currency: data.currency,
+                    items: data.content_ids ? data.content_ids.map(id => ({ item_id: id })) : []
+                }
+            });
         }
     }
 });
 </script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id={{ $pixelId }}&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
-@endif
